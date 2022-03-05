39972ae87e2a19afed4b9386fbb3718ff315d86a
sara7470 <sara7470@yahoo.com>
2022-03-05 12:28:28 +0300

    c2-cw1 done

---
+++ c2-cw1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Aether";
+            string superPower1 = "fireBalls";
+            string superPower2 = "armor";
+            int age = 17;
+            double speed = 1.5;
+
+                Console.WriteLine("My name is " + characterName + " and my super powers are " + superPower1 + " and " + superPower2 + ". My age is " + age + " and my speed is " + speed);
+
+            
+        }
+    }
+}
